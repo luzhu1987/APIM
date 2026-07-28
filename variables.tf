08c5b359f79ce6e4c14c6b0f4bc99fa9198ccb72
lu zhu <zhulu1987@outlook.com>
2026-07-28 23:22:02 +0800

    Add initial Terraform configuration for Azure API Management setup
    - Create main.tf with resources for random pet and Azure resource group
    - Define variables in variables.tf for resource group location, name prefix, publisher details, SKU name, and SKU count
    - Initialize outputs.tf and providers.tf for Terraform setup

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location for all resources."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "publisher_email" {
+  default     = "test@contoso.com"
+  description = "The email address of the owner of the service"
+  type        = string
+  validation {
+    condition     = length(var.publisher_email) > 0
+    error_message = "The publisher_email must contain at least one character."
+  }
+}
+
+variable "publisher_name" {
+  default     = "publisher"
+  description = "The name of the owner of the service"
+  type        = string
+  validation {
+    condition     = length(var.publisher_name) > 0
+    error_message = "The publisher_name must contain at least one character."
+  }
+}
+
+variable "sku_name" {
+  description = "The pricing tier of this API Management service"
+  default     = "Developer"
+  type        = string
+  validation {
+    condition     = contains(["Basic", "BasicV2", "Consumption", "Developer", "Premium", "PremiumV2", "Standard", "StandardV2"], var.sku_name)
+    error_message = "The sku must be one of the following: Basic, BasicV2, Consumption, Developer, Premium, PremiumV2, Standard, StandardV2."
+  }
+}
+
+variable "sku_count" {
+  description = "The instance size of this API Management service."
+  default     = 1
+  type        = number
+  validation {
+    condition     = contains([1, 2], var.sku_count)
+    error_message = "The sku_count must be one of the following: 1, 2."
+  }
+}
